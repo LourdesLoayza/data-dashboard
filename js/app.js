fde75b68fe33b447ec5746fca7dd4ec363731a8d
LourdesLoayza <30554218+LourdesLoayza@users.noreply.github.com>
2017-11-30 08:11:03 -0500

    select enlazado

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 // Declaración de variables generales
 
-var sedeAqp = data.AQP;
-var sedeLim = data.LIM;
-var sedeScl = data.SCL;
-var sedeCdmx = data.CDMX;
+var promAqp = data.AQP;
+var promLim = data.LIM;
+var promScl = data.SCL;
+var promCdmx = data.CDMX;
 
-var sedes = Object.keys(data);
-var promociones = Object.keys(sedes);
+// var sedes = Object.keys(data);
+// var promociones = Object.keys(sedes);
   
 var prom20162 = '2016-2';
 var prom20171 = '2017-1';
@@ -31,40 +31,39 @@ function begin() {
 
   // Declaración de variables indicadores Students
   var studentsDetailsValue = document.getElementById('students-details');
- 
 
+  var sede;
+  var promocion;
+ 
   // Función selección de sede
   function selectionSede(event) {
     switch (true) {
-    case event.target.value === 'LIM':
-      sede = sedeLim;
-      break;
     case event.target.value === 'AQP':
-      console.log(sedeAqp);
-      break;
-    case event.target.value === 'SCL':
-      console.log(sedeScl);
+      sede = Object.keys(data)[0];
       break;
     case event.target.value === 'CDMX':
-      console.log(sedeCdmx);
+      sede = Object.keys(data)[1];
+      break;
+    case event.target.value === 'LIM':
+      sede = Object.keys(data)[2];
+      break;
+    case event.target.value === 'SCL':
+      sede = Object.keys(data)[3];
       break;
     }
   }
-    
+
   // Función selección de promoción
   function selectionProm(event) {
     switch (true) {
-    case event.target.value === '':
-      console.log('prueba');
-      break;
     case event.target.value === '20162':
-      console.log(prom20162);
+      promocion = prom20162;
       break;
     case event.target.value === '20171':
-      console.log(prom20171);
+      promocion = prom20171;
       break;
     case event.target.value === '20172':
-      console.log(prom20172);
+      promocion = prom20172;
       break;
     }
   }
@@ -72,55 +71,83 @@ function begin() {
 
   // Función mostrar datos overview
   function showOverviewData(selectSede, selectProm) {
+    debugger;
+    switch (true) {
+    case (sede === 'AQP') && (promocion === '2016-2'):
+      console.log('Sede AQP Promocion 2016-2');
+      break;
+    case (sede === 'AQP') && (promocion === '2017-1'):
+      console.log('Sede AQP Promocion 2017-1');
+      break;
+    case (sede === 'CDMX') && (promocion === '2017-1'):
+      console.log('Sede CDMX Promocion 2017-1');
+      break;
+    case (sede === 'CDMX') && (promocion === '2017-2'):
+      console.log('Sede CDMX Promocion 2017-2');
+      break;
+    case (sede === 'LIM') && (promocion === '2016-2'):
+      console.log('Sede LIM Promocion 2017-2');
+      break;
+    case (sede === 'LIM') && (promocion === '2017-1'):
+      console.log('Sede LIM Promocion 2017-2');
+      break;
+    case (sede === 'LIM') && (promocion === '2017-2'):
+      console.log('Sede LIM Promocion 2017-2');
+      break;
+    case (sede === 'SCL') && (promocion === '2016-2'):
+      console.log('Sede LIM Promocion 2017-2');
+      break;
+    case (sede === 'SCL') && (promocion === '2017-1'):
+      console.log('Sede LIM Promocion 2017-2');
+      break;
+    case (sede === 'SCL') && (promocion === '2017-2'):
+      console.log('Sede LIM Promocion 2017-2');
+      break;
+    }
+
     // Muestra la información de indicadores Overview data
     
     // Los valores de NPS cuentan con sub indicadores
     // Valores información: NPS Promoters
     var dataNpsPromoters = document.createElement('p');
-    dataNpsPromoters.textContent = 'Promoters: ' + sedeAqp[prom20162][ratings][0].nps['promoters'] + '%';
+    dataNpsPromoters.textContent = 'Promoters: ' + promAqp[prom20162][ratings][0].nps['promoters'] + '%';
     npsValue.appendChild(dataNpsPromoters);
 
     // Valores información: NPS Passive
     var dataNpsPassive = document.createElement('p');
-    dataNpsPassive.textContent = 'Passive: ' + sedeAqp[prom20162][ratings][0].nps['passive'] + '%';
+    dataNpsPassive.textContent = 'Passive: ' + promAqp[prom20162][ratings][0].nps['passive'] + '%';
     npsValue.appendChild(dataNpsPassive);
 
     // Valores información: NPS Detractors
     var dataNpsDetractors = document.createElement('p');
-    dataNpsDetractors.textContent = 'Detractors: ' + sedeAqp[prom20162][ratings][0].nps['detractors'] + '%';
+    dataNpsDetractors.textContent = 'Detractors: ' + promAqp[prom20162][ratings][0].nps['detractors'] + '%';
     npsValue.appendChild(dataNpsDetractors);
 
     // Los valores de Student cuentan con sub indicadores
     // Valores información Student Status: No cumple
     var dataStudentsNocumple = document.createElement('p');
-    dataStudentsNocumple.textContent = 'No cumple: ' + sedeAqp[prom20162][ratings][0].student['no-cumple'] + '%';
+    dataStudentsNocumple.textContent = 'No cumple: ' + promAqp[prom20162][ratings][0].student['no-cumple'] + '%';
     studentsValue.appendChild(dataStudentsNocumple);
 
     // Valores información Student Status: Cumple
     var dataStudentsCumple = document.createElement('p');
-    dataStudentsCumple.textContent = 'Cumple: ' + sedeAqp[prom20162][ratings][0].student['cumple'] + '%';
+    dataStudentsCumple.textContent = 'Cumple: ' + promAqp[prom20162][ratings][0].student['cumple'] + '%';
     studentsValue.appendChild(dataStudentsCumple);
 
     // Valores información Student Status: Supera
     var dataStudentsSupera = document.createElement('p');
-    dataStudentsSupera.textContent = 'Supera: ' + sedeAqp[prom20162][ratings][0].student['supera'] + '%';
+    dataStudentsSupera.textContent = 'Supera: ' + promAqp[prom20162][ratings][0].student['supera'] + '%';
     studentsValue.appendChild(dataStudentsSupera);
 
     // Valores información ranking teacher
     var dataTeacher = document.createElement('p');
-    dataTeacher.textContent = sedeAqp[prom20162][ratings][0].teacher + '%';
+    dataTeacher.textContent = promAqp[prom20162][ratings][0].teacher + '%';
     teacherValue.appendChild(dataTeacher);
 
     // Valores información ranking jedi
     var dataJedi = document.createElement('p');
-    dataJedi.textContent = sedeAqp[prom20162][ratings][0].jedi + '%';
+    dataJedi.textContent = promAqp[prom20162][ratings][0].jedi + '%';
     jediValue.appendChild(dataJedi);
-
-    // switch (true) {
-    // case (selectSede.target.value === 'LIM') && (selectProm.target.value === '20162'):
-    //   console.log('prueba');
-    //   break;
-    // }
   }
 
   // Función mostrar datos students
@@ -147,7 +174,7 @@ function begin() {
   
   btnOverviewData.addEventListener('click', showOverviewData);
   btnStudentsData.addEventListener('click', showStudentsData);
-  selectSede.addEventListener('change', selectionProm);
+  selectSede.addEventListener('change', selectionSede);
   selectProm.addEventListener('change', selectionProm);
 };
 
